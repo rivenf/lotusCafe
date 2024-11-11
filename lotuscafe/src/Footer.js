865575432aaa2b50f0267692f784e6470ed7ac27
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-11-11 18:34:03 +0000

    Add social media icons

--- lotuscafe/src/Footer.js
+++ lotuscafe/src/Footer.js
@@ -1,7 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   return (
     <footer>
-      <div className="socials">(social icons go here)</div>
+      <div className="socials">
+        <a href="https://www.facebook.com" target="_blank">
+          <FontAwesomeIcon icon={faSquareFacebook} />
+        </a>
+        <a href="https://www.instagram.com" target="_blank">
+          <FontAwesomeIcon icon={faInstagramSquare} />
+        </a>
+        <a href="https://www.x.com" target="_blank">
+          <FontAwesomeIcon icon={faSquareXTwitter} />
+        </a>
+      </div>
       <p>Lotus Cafe 2024</p>
       <p>
         1234 Placeholder St.
